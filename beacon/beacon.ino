3e915c559070169b82ee734bce462f570e958a7d
bsanzi <brunosanzi@virgilio.it>
2024-11-27 12:02:58 +0100

    beacon scanner ok

--- beacon/beacon.ino
+++ beacon/beacon.ino
@@ -1,27 +1,13 @@
 //ESP32 and try it out now!
  
-
 #include <BLEDevice.h>            // sets up BLE device constructs<font></font>
 #include <BLEUtils.h>             // various BLE utilities for processing BLE data<font></font>
 #include <BLEScan.h>              // contains BLE scanning functions<font></font>
 #include <BLEAdvertisedDevice.h>  // contains BLE device characteristic data<font></font>
+#include <config.h>
 
 
-
-const int RED = 4;
-const int YELLOW = 16;
-const int GREEN = 17;
- 
-
-const int FAULT_ZONE = -65;
-const int WARNING_ZONE = -72;
- 
-
-const int DEFAULT_RSSI = -150;
- 
-
-String my_beacon_address = "e5:31:d2:ed:02:8b";
- 
+int32_t buffer[4] = {0,0,0,0};
 
 void setup(){
 pinMode(RED, OUTPUT);
@@ -29,54 +15,69 @@ pinMode(YELLOW, OUTPUT);
 pinMode(GREEN, OUTPUT);
 BLEDevice::init("");
 Serial.begin(baud_rate);
-Serial.println("go");
- 
-
+Serial.println("serial port ready");
 }
  
 
 void loop(){
+static int idx;
+
 BLEScan* scan = BLEDevice::getScan();
-scan-> setActiveScan(true);
-    pBLEScan->setActiveScan(true); // Modalità attiva: richiede più dati dai beacon
-    pBLEScan->setInterval(100);    // Intervallo tra scansioni (ms)
-    pBLEScan->setWindow(100);       // Finestra di scansione (ms)
-BLEScanResults* results= scan->start(1);
-int RSSI=DEFAULT_RSSI;
+    scan-> setActiveScan(true);     // Modalità attiva: richiede più dati dai beacon
+    scan-> setInterval(100);    // Intervallo tra scansioni (ms)
+    scan-> setWindow(100);       // Finestra di scansione (ms)
+
+BLEScanResults* results= scan->start(1,false);
+delay(100);
+scan->stop();
+
+int RSSI=FREE_ZONE;
+int rssi;
 String name;
 String address;
- 
 
-for(int i=0; i <results->getCount();i++){
- 
-  BLEAdvertisedDevice device=results->getDevice(i);
-  address= device.getAddress().toString();
-  if (address == my_beacon_address ) {
-  int32_t rssi = device.getRSSI();
-  name= device.getName();
-    RSSI=rssi;
+
+  for(int i=0; i <results->getCount();i++){
+    BLEAdvertisedDevice device=results->getDevice(i);
+    address= device.getAddress().toString();
+      if (address == my_beacon_address ) {
+      rssi = device.getRSSI();
+      name= device.getName();
+      idx++;
+      }
   }
-}
- if (RSSI>FAULT_ZONE){
-  digitalWrite(RED, RSSI>FAULT_ZONE ? HIGH:LOW);
-  digitalWrite(YELLOW,LOW);
-  digitalWrite(GREEN,LOW);
-  Serial.print("Fault con: ");
-  Serial.print(RSSI);
-  Serial.println(" dbm");
-  Serial.println(name);
-  Serial.println(address);
- }
-    else if (RSSI<FAULT_ZONE && RSSI>WARNING_ZONE){
-    digitalWrite(YELLOW, RSSI<FAULT_ZONE ? HIGH:LOW);
-    digitalWrite(RED,LOW);
-    digitalWrite(GREEN,LOW);
-    Serial.print("Warning con: ");
-    Serial.print(RSSI);
-    Serial.println(" dbm");
-    Serial.println(name);
-    Serial.println(address);
-    }
+
+      if (idx<4){
+      buffer[idx]=rssi;
+      }
+      else {
+        idx=0;
+        buffer[0]=rssi;
+      }
+      RSSI= ((buffer[0]+buffer[1]+buffer[2]+buffer[3])/4);
+                 Serial.println(idx);
+
+      if (RSSI>FAULT_ZONE){
+      digitalWrite(RED, RSSI>FAULT_ZONE ? HIGH:LOW);
+      digitalWrite(YELLOW,LOW);
+      digitalWrite(GREEN,LOW);
+      Serial.print("Fault con: ");
+      Serial.print(RSSI);
+      Serial.println(" dbm");
+      Serial.println(name);
+      Serial.println(address);
+
+      }
+        else if (RSSI<FAULT_ZONE && RSSI>WARNING_ZONE){
+        digitalWrite(YELLOW, RSSI<FAULT_ZONE ? HIGH:LOW);
+        digitalWrite(RED,LOW);
+        digitalWrite(GREEN,LOW);
+        Serial.print("Warning con: ");
+        Serial.print(RSSI);
+        Serial.println(" dbm");
+        Serial.println(name);
+        Serial.println(address);
+        }
   else {
   digitalWrite(GREEN,HIGH);
   digitalWrite(YELLOW,LOW);
--- beacon/config.h
+++ beacon/config.h
@@ -1,16 +1,16 @@
-int baud_rate=115200;
 
-const int RED = 4;
-const int YELLOW = 16;
-const int GREEN = 17;
+const int RED = 4;      // GPIO PIN
+const int YELLOW = 16;  // GPIO PIN
+const int GREEN = 17;   // GPIO PIN
  
 
-const int FAULT_ZONE = -65;
-const int WARNING_ZONE = -72;
- 
+const int FAULT_ZONE = -65;     // dbm
+const int WARNING_ZONE = -75;   // dbm
+const int FREE_ZONE = -150; // dbm
 
-const int DEFAULT_RSSI = -150;
+const int baud_rate = 115200;
  
 
 String my_beacon_address = "e5:31:d2:ed:02:8b";
+
  
